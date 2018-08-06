fe46481f953f0e8e08c911c2fc12a7fa79c6b4c2
Daniel Schruhl <danielschruhl@gmail.com>
2018-08-06 15:58:05 +0200

    add amazon s3 client

--- src/main/scala/Target.scala
+++ src/main/scala/Target.scala
@@ -1,7 +1,12 @@
 import java.security.MessageDigest
 
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+
 object Target {
-  val bucketPrefix = "robocop-"
+
+  val bucketPrefix: String = "robocop-"
+  val s3: AmazonS3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build()
 
   def bucketName(filePath: String): String = {
     val study = Sink.studyName(filePath)
