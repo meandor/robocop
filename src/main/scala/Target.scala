53f1d19e8fae4335c7aa4293cee59f26100f620b
Daniel Schruhl <danielschruhl@gmail.com>
2018-08-06 16:46:12 +0200

    add function to upload to s3 in multipart

--- src/main/scala/Target.scala
+++ src/main/scala/Target.scala
@@ -1,15 +1,18 @@
 import java.io.File
 import java.security.MessageDigest
 
-import Robocop.ShoppingList
+import Sink.TransferItem
+import com.amazonaws.AmazonServiceException
 import com.amazonaws.regions.Regions
-import com.amazonaws.services.s3.transfer.TransferManagerBuilder
+import com.amazonaws.services.s3.transfer.{TransferManager, TransferManagerBuilder}
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+import com.typesafe.scalalogging.LazyLogging
 
-object Target {
+object Target extends LazyLogging {
 
   val bucketPrefix: String = "robocop-"
   val s3: AmazonS3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build()
+  val multipartThreshold: Long = 100 * 1024 * 1025
 
   def bucketName(filePath: String): String = {
     val study = Sink.studyName(filePath)
@@ -17,19 +20,29 @@ object Target {
       MessageDigest.getInstance("MD5").digest(study.getBytes).map("%02x".format(_)).mkString
   }
 
-//  def uploadFileToS3(localFilePath: String, originalFilePath: String) = {
-//    val localFile = new File(localFilePath)
-//    val localFileBucket = bucketName()
-//    val transferManager = TransferManagerBuilder.standard().build()
-//
-//    try {
-//      MultipleFileUpload xfer = transferManager.uploadFileList(bucket_name,
-//        key_prefix, new File("."), files);
-//      XferMgrProgress.showTransferProgress(xfer);
-//      XferMgrProgress.waitForCompletion(xfer);
-//    } catch (AmazonServiceException e) {
-//      System.err.println(e.getErrorMessage());
-//      System.exit(1);
-//    }
-//  }
+  def uploadFileToS3(transferItem: TransferItem): Boolean = {
+    val localFile = new File(transferItem.localPath)
+    val targetBucket = bucketName(transferItem.originalPath)
+
+    if (!s3.doesBucketExistV2(targetBucket)) {
+      throw new IllegalArgumentException(s"Bucket ($targetBucket) does not exist")
+    }
+
+    val key = transferItem.localPath.split("/").last
+    val transferManager: TransferManager = TransferManagerBuilder.standard().withS3Client(s3).withMultipartUploadThreshold(multipartThreshold).build()
+
+    try {
+      val transfer = transferManager.upload(targetBucket, key, localFile)
+      TransferManagerProgress.showTransferProgress(transfer)
+      TransferManagerProgress.waitForCompletion(transfer)
+      true
+    } catch {
+      case ex: AmazonServiceException =>
+        logger.error(ex.getErrorMessage)
+        false
+      case ex: Throwable =>
+        logger.error(ex.getMessage, ex)
+        false
+    }
+  }
 }
---
+++ src/main/scala/TransferManagerProgress.scala
@@ -0,0 +1,65 @@
+import com.amazonaws.services.s3.transfer._
+import com.amazonaws.{AmazonClientException, AmazonServiceException}
+import com.typesafe.scalalogging.LazyLogging
+
+object TransferManagerProgress extends LazyLogging { // waits for the transfer to complete, catching any exceptions that occur.
+
+  def waitForCompletion(transfer: Transfer): Unit = {
+    try
+      transfer.waitForCompletion()
+    catch {
+      case e: AmazonServiceException =>
+        logger.error("Amazon service error: " + e.getMessage, e)
+      case e: AmazonClientException =>
+        logger.error("Amazon client error: " + e.getMessage, e)
+      case e: InterruptedException =>
+        logger.error("Transfer interrupted: " + e.getMessage, e)
+    }
+  }
+
+  // Prints progress while waiting for the transfer to finish.
+  def showTransferProgress(xfer: Transfer): Unit = {
+    // print the transfer's human-readable description
+    logger.info(xfer.getDescription)
+    // print an empty progress bar...
+    printProgressBar(0.0)
+    // update the progress bar while the xfer is ongoing.
+    do {
+      try
+        Thread.sleep(100)
+      catch {
+        case _: InterruptedException =>
+          return
+      }
+      // Note: so_far and total aren't used, they're just for
+      // documentation purposes.
+      val progress = xfer.getProgress
+      val so_far = progress.getBytesTransferred
+      val total = progress.getTotalBytesToTransfer
+      val pct = progress.getPercentTransferred
+      eraseProgressBar()
+      printProgressBar(pct)
+    } while ( {
+      !xfer.isDone
+    })
+    // print the final state of the transfer.
+    val xfer_state = xfer.getState
+    logger.info(": " + xfer_state)
+  }
+
+  // prints a simple text progressbar: [#####     ]
+  def printProgressBar(pct: Double): Unit = { // if bar_size changes, then change erase_bar (in eraseProgressBar) to
+    // match.
+    val bar_size = 40
+    val empty_bar = "                                        "
+    val filled_bar = "########################################"
+    val amt_full = (bar_size * (pct / 100.0)).toInt
+    logger.info("  [%s%s]".format(filled_bar.substring(0, amt_full), empty_bar.substring(0, bar_size - amt_full)))
+  }
+
+  // erases the progress bar.
+  def eraseProgressBar(): Unit = { // erase_bar is bar_size (from printProgressBar) + 4 chars.
+    val erase_bar = "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
+    logger.info(erase_bar.format())
+  }
+}
