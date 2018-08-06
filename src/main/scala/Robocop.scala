46d1cd0ca1cd77e2be63843391c6ce3e06a08ff5
Daniel Schruhl <danielschruhl@gmail.com>
2018-08-06 13:05:27 +0200

    copy existing files to local storage

--- src/main/scala/Robocop.scala
+++ src/main/scala/Robocop.scala
@@ -1,13 +1,34 @@
-import java.io.FileNotFoundException
+import java.io.{File, FileNotFoundException}
 
 import com.typesafe.scalalogging.LazyLogging
 
+import scala.collection.parallel.ParSeq
 import scala.io.Source
+import scala.language.postfixOps
 
 object Robocop extends LazyLogging {
 
-  def parseShoppingList(shoppingListFilePath: String): Seq[String] = {
-    Source.fromFile(shoppingListFilePath).getLines.filter(line => line != "").toSeq
+  type ShoppingList = ParSeq[String]
+
+  def download(url: String, filename: String) = {
+    import java.net.URL
+
+    import sys.process._
+    new URL(url) #> new File(filename) !!
+  }
+
+  def downloadFile(url: String, tmpFolder: String): String = {
+    val filename = tmpFolder + "/" + url.split("/").last
+    download(url, filename)
+    filename
+  }
+
+  def copyToLocal(shoppingList: ShoppingList, tmpFolder: String): ParSeq[String] = {
+    shoppingList.map(shoppingListItem => downloadFile(shoppingListItem, tmpFolder))
+  }
+
+  def parseShoppingList(shoppingListFilePath: String): ShoppingList = {
+    Source.fromFile(shoppingListFilePath).getLines.filter(line => line != "").toSeq.par
   }
 
   def main(args: Array[String]): Unit = {
@@ -17,10 +38,14 @@ object Robocop extends LazyLogging {
     } else {
       try {
         val syncingUser = arguments.head
-        val toBeSyncedFiles = parseShoppingList(arguments.last)
+        val shoppingList = parseShoppingList(arguments.last)
 
         logger.info(s"Starting syncing as user: $syncingUser")
-        logger.info(s"Syncing files: " + toBeSyncedFiles.mkString("\n"))
+        logger.info(s"Syncing files: " + shoppingList.mkString("\n"))
+
+        val localShoppingListItems = copyToLocal(shoppingList, s"./tmp/${System.currentTimeMillis()}")
+
+
       } catch {
         case e: FileNotFoundException => logger.warn("The specified shoppingList could not be found/accessed")
         case e: Throwable => logger.error(e.getMessage)
@@ -28,3 +53,14 @@ object Robocop extends LazyLogging {
     }
   }
 }
+
+//  val s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build()
+//
+//  val buckets = s3.listBuckets()
+//  System.out.println("Your Amazon S3 buckets are:")
+//
+//  import scala.collection.JavaConversions._
+//
+//  for (b <- buckets) {
+//    System.out.println("* " + b.getName)
+//  }
--- src/test/scala/RobocopTest.scala
+++ src/test/scala/RobocopTest.scala
@@ -1,4 +1,5 @@
 import java.io.FileNotFoundException
+import java.nio.file.{Files, Paths}
 
 import org.scalatest.{FeatureSpec, Matchers}
 
@@ -10,7 +11,19 @@ class RobocopTest extends FeatureSpec with Matchers {
     }
 
     scenario("shopping list file does not exist") {
-      an [FileNotFoundException] should be thrownBy Robocop.parseShoppingList("src/test/resources/foo")
+      an[FileNotFoundException] should be thrownBy Robocop.parseShoppingList("src/test/resources/foo")
+    }
+  }
+
+  feature("Copy shopping list files locally") {
+    scenario("copy one file") {
+      val downloadFolder = "src/test/resources/tmp"
+      val expectedDownloadedFile = "src/test/resources/tmp/Example.jpg"
+      Files.deleteIfExists(Paths.get(expectedDownloadedFile))
+      val copiedFiles = Robocop.copyToLocal(Seq("https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg").par, downloadFolder)
+
+      copiedFiles should contain theSameElementsAs Seq(expectedDownloadedFile)
+      Files.exists(Paths.get(expectedDownloadedFile)) shouldBe true
     }
   }
 }
